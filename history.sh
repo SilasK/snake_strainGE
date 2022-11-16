f23460e4e58106f9558090f91f1f7aa9943c7fe3
silask <silas.kieser@gmail.com>
2022-11-16 14:12:32 +0100

    dev scripts

---
+++ history.sh
@@ -0,0 +1,20 @@
+
+mamba install strainge
+mamba create -n download_genome  ncbi-genome-download
+
+
+
+mkdir ref_genomes
+ncbi-genome-download bacteria -l complete -g Staphylococcus,Streptococcus -H -F all -o ref_genomes -P -p 4
+
+wget https://github.com/broadinstitute/StrainGE/blob/master/bin/prepare_strainge_db.py
+
+
+mkdir strainge_db
+python3 prepare_strainge_db.py ref_genomes/human_readable -s \
+    -o strainge_db > strainge_db/references_meta.tsv
+
+for f in strainge_db/*.fa.gz; do straingst kmerize -o ${f%.fa.gz}.hdf5 $f; done;
+
+
+mamba install -y mummer 
